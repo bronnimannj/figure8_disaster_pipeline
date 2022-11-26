32a6424e0c434e57bb154dc67ebd1d0f60fd0e2b
jsbinkywriter453 <60605350+jsbinkywriter453@users.noreply.github.com>
2022-11-26 21:58:58 +0000

    update ml part

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -37,11 +37,11 @@ def load_data(database_filepath):
     It then defines and returns feature and target variable X and Y.
     
     Args:
-    database_filepath: path where the database is
+        database_filepath: path where the database is
 
     Returns:
-    X: features
-    y: targets
+        X: features
+        y: targets
     """
 
     engine = create_engine('sqlite:///../data/' + database_filepath)
@@ -63,10 +63,10 @@ def tokenize(text):
     This function processes the message
     
     Args:
-    text: list of text messages
+        text: list of text messages
 
     Returns:
-    clean_tokens: tokenized text
+        clean_tokens: tokenized text
     """
 
     # First remove punctation and lowercase all letters
@@ -103,32 +103,141 @@ class TextLengthExtractor(BaseEstimator, TransformerMixin):
 
 def build_model():
     """
-    Returns a machine learning pipeline that process text and then performs
+    This function returns a sklearn pipeline that process text and then performs
     multi-output classification on the categories in the dataset.
 
     Args:
-    None
+        None
 
     Returns:
-    a scikit learn pipeline model
+        pipeline: a sklearn pipeline model
     """
 
-    pipeline = Pipeline(
-        # ...
+    pipeline = Pipeline([
+        (
+            'features', 
+            FeatureUnion([
+                ('text_pipeline', Pipeline([
+                    (
+                        'vect', 
+                        CountVectorizer(
+                            tokenizer = tokenize,
+                            max_df=0.75)),
+                    ('tfidf', TfidfTransformer(sublinear_tf = False)) ])),
+                ('text_length', TextLengthExtractor()) ]
+            )),
+        (
+            'clf', 
+            MultiOutputClassifier(
+                RandomForestClassifier(
+                    n_estimators = 500,
+                    min_samples_leaf = 1)))
+    ])
+
+    return pipeline
+
+def setup_logger(logger_name, log_file, level=logging.INFO):
+    """
+    This function sets up a new logging file.
+
+    Args:
+        logger_name : name of the logger
+        log_file    : name of the file where to save it
+        level       : base logging level to be used for this logger
+
+    Returns:
+        None
+    """
+    l = logging.getLogger(logger_name)
+    formatter = logging.Formatter('%(message)s') # %(asctime)s - %(levelname)s - 
+    fileHandler = logging.FileHandler(log_file, mode='w')
+    fileHandler.setFormatter(formatter)
+    streamHandler = logging.StreamHandler()
+    streamHandler.setFormatter(formatter)
+
+    l.setLevel(level)
+    l.addHandler(fileHandler)
+    l.addHandler(streamHandler) 
+
+def close_logger(logger):
+    """
+    
+    This function closes all handlers on logger object.
+    
+    Args:
+        logger : logger object
+
+    Returns:
+        None
+    
+    """
+    if logger is None:
+        return
+    for handler in list(logger.handlers):
+        handler.close()
+        logger.removeHandler(handler)    
+
+def evaluate_model(model, X_test, Y_test, category_names, name_experiment):
+    """
+    This function prints the performance of a pipeline for all the categories that we're trying to predict.
+    
+    Args:
+        model           : The model that we will use to make the predictions with
+        X_test          : Attributes that we will use to make the predictions
+        Y_test          : True labels for the corresponding attributes in X_test
+        category_names  : list of categories that can be predictied
+        name_experiment : The name of the experiment and logging file
+
+    Returns:
+        None
+    """
+
+    y_prediction = model.predict(X_test)
+
+    setup_logger(
+        'log_pipeline', 
+        f'logs/{name_experiment}_log.txt'
     )
+    logger = logging.getLogger('log_pipeline')
 
-    pass
+    for idx, column in enumerate(y_test.columns):
+        logger.info('feature predicted: ' + column)
+        logger.info('-'*53)
+        logger.info(classification_report(y_test[column], predictions[:,idx], zero_division = 0))
+        logger.info("\n")
+    close_logger(logger)   
 
-
-def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    return None
 
 
 def save_model(model, model_filepath):
-    pass
+    """
+    This function saves the trained model into a specific (pickle) file.
+
+    Args:
+
+        model          : The trained model
+        model_filepath : (relative) path where to store the model
+
+    Returns:
+        None
+    """
+
+    model.to_pickle(model_filepath)
+    return None
 
 
 def main():
+    """
+    This function is the main one that will run the whole pipeline.
+
+    Args:
+        None
+
+    Returns:
+        None
+    """
+
     if len(sys.argv) == 3:
         database_filepath, model_filepath = sys.argv[1:]
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
@@ -142,49 +251,25 @@ def main():
         model.fit(X_train, Y_train)
         
         print('Evaluating model...')
-        evaluate_model(model, X_test, Y_test, category_names)
+        evaluate_model(
+            model, 
+            X_test, 
+            Y_test, 
+            category_names,
+            name_experiment = 'second_experiment'
+        )
 
         print('Saving model...\n    MODEL: {}'.format(model_filepath))
         save_model(model, model_filepath)
 
-        print('Trained model saved!')
-
+        print('Trained model saved!') 
     else:
-        print('Please provide the filepath of the disaster messages database '\
-              'as the first argument and the filepath of the pickle file to '\
-              'save the model to as the second argument. \n\nExample: python '\
-              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
-
+        print(
+            """Please provide :
+            - the filepath of the disaster messages database as the first argument 
+            - the filepath of the pickle file save the model to as the second argument. 
+            
+            Example: python train_classifier.py ../data/disaster_response.db classifier.pkl""")
 
 if __name__ == '__main__':
     main()
-
-
-# def train(X, y, model):
-#     # train test split
-
-
-#     # fit model
-
-
-#     # output model test results
-
-
-#     return model
-
-
-# def export_model(model):
-#     # Export model as a pickle file
-
-
-
-# def run_pipeline(data_file):
-#     X, y = load_data(data_file)  # run ETL pipeline
-#     model = build_model()  # build model pipeline
-#     model = train(X, y, model)  # train model pipeline
-#     export_model(model)  # save model
-
-
-# if __name__ == '__main__':
-#     data_file = sys.argv[1]  # get filename of dataset
-#     run_pipeline(data_file)  # run data pipeline
